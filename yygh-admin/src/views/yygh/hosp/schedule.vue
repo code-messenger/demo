138dfbad660a59efb4c952e69a55f35fb6b0ac15
beifeng <2842996877@qq.com>
2022-05-15 20:33:17 +0800

    格式化文件

--- yygh-admin/src/views/yygh/hosp/schedule.vue
+++ yygh-admin/src/views/yygh/hosp/schedule.vue
@@ -2,93 +2,43 @@
   <div class="app-container">
     <div style="margin-bottom: 10px; font-size: 10px">选择：</div>
     <el-container style="height: 100%">
-      <el-aside
-        width="200px"
-        style="border: 1px silver solid"
-      >
+      <el-aside width="200px" style="border: 1px silver solid">
         <!-- 部门 -->
-        <el-tree
-          :data="data"
-          :props="defaultProps"
-          :default-expand-all="true"
-          @node-click="handleNodeClick"
-        ></el-tree>
+        <el-tree :data="data" :props="defaultProps" :default-expand-all="true" @node-click="handleNodeClick"></el-tree>
       </el-aside>
       <el-main style="padding: 0 0 0 20px">
         <el-row style="width: 100%">
           <!-- 排班日期 分页 -->
-          <el-tag
-            v-for="(item, index) in bookingScheduleList"
-            :key="item.id"
-            @click="selectDate(item.workDate, index)"
-            :type="index == activeIndex ? '' : 'info'"
-            style="
+          <el-tag v-for="(item, index) in bookingScheduleList" :key="item.id" @click="selectDate(item.workDate, index)"
+            :type="index == activeIndex ? '' : 'info'" style="
               height: 60px;
               margin-right: 5px;
               margin-right: 15px;
-              cursor: pointer;"
-          >
+              cursor: pointer;">
             {{ item.workDate }} {{ item.dayOfWeek }}
             <br />
             {{ item.availableNumber }} / {{ item.reservedNumber }}
           </el-tag>
           <!-- 分页 -->
-          <el-pagination
-            :current-page="page"
-            :total="total"
-            :page-size="limit"
-            class="pagination"
-            layout="prev, pager, next"
-            @current-change="getPage"
-          ></el-pagination>
+          <el-pagination :current-page="page" :total="total" :page-size="limit" class="pagination"
+            layout="prev, pager, next" @current-change="getPage"></el-pagination>
         </el-row>
         <el-row style="margin-top: 20px">
           <!-- 排班日期对应的排班医生 -->
-          <el-table
-            v-loading="listLoading"
-            :data="scheduleList"
-            border
-            fit
-            highlight-current-row
-          >
-            <el-table-column
-              label="序号"
-              width="60"
-              align="center"
-            >
+          <el-table v-loading="listLoading" :data="scheduleList" border fit highlight-current-row>
+            <el-table-column label="序号" width="60" align="center">
               <template slot-scope="scope">{{ scope.$index + 1 }}</template>
             </el-table-column>
-            <el-table-column
-              label="职称"
-              width="150"
-            >
+            <el-table-column label="职称" width="150">
               <template slot-scope="scope">{{ scope.row.title }} | {{ scope.row.docname }}</template>
             </el-table-column>
-            <el-table-column
-              label="号源时间"
-              width="80"
-            >
-              <template slot-scope="scope">{{scope.row.workTime == 0 ? "上午" : "下午"}}</template>
+            <el-table-column label="号源时间" width="80">
+              <template slot-scope="scope">{{ scope.row.workTime == 0 ? "上午" : "下午" }}</template>
             </el-table-column>
-            <el-table-column
-              prop="reservedNumber"
-              label="可预约数"
-              width="80"
-            />
-            <el-table-column
-              prop="availableNumber"
-              label="剩余预约数"
-              width="100"
-            />
-            <el-table-column
-              prop="amount"
-              label="挂号费(元)"
-              width="90"
-            />
-            <el-table-column
-              prop="skill"
-              label="擅长技能"
-            />
+            <el-table-column prop="reservedNumber" label="可预约数" width="80" />
+            <el-table-column prop="availableNumber" label="剩余预约数" width="100" />
+            <el-table-column prop="amount" label="挂号费(元)" width="90" />
+            <el-table-column prop="skill" label="擅长技能" />
           </el-table>
         </el-row>
       </el-main>
@@ -205,12 +155,12 @@ export default {
 </script>
 
 <style>
-.el-tree-node.is-current > .el-tree-node__content {
+.el-tree-node.is-current>.el-tree-node__content {
   background-color: #409eff !important;
   color: white;
 }
 
-.el-checkbox__input.is-checked + .el-checkbox__label {
+.el-checkbox__input.is-checked+.el-checkbox__label {
   color: black;
 }
 </style>
